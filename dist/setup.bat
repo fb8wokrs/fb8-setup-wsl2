3d636f95cfe86488ac7f088df828a4c11c38f6ef
Daisuke Arai <daisuke@LGDA>
2023-01-11 06:28:45 +0900

    不要なものを削除

--- dist/setup.bat
+++
@@ -1,5 +0,0 @@
-@REM -*- encoding: cp932-dos -*-
-@echo "WSL2 のセットアップを行います。"
-PowerShell -Command "Set-ExecutionPolicy Bypass -Scope Process; .\scripts\setup.ps1"
-@echo "何かキーを押すとこのウィンドウを閉じます。"
-@pause
